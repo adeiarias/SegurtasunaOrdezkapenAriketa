e143d7346cd9471d1a1e7771ea536cc5b573d28c
adeiarias <adeiariasupv@gmail.com>
2020-09-23 18:45:54 +0200

    Aldaketa batzuk igo

--- src/main/java/segurtasuna/kripto_ordezkapena.java
+++ src/main/java/segurtasuna/kripto_ordezkapena.java
@@ -4,11 +4,12 @@ import java.util.Arrays;
 
 
 import java.util.Arrays;
+import java.util.HashSet;
 
-    public class kripto_ordezkapena {
+public class kripto_ordezkapena {
         private String emaitza;
         private Character[] alfabetoa = new Character[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-        private String gakoa = " ";
+        private String gakoa;
 
         public kripto_ordezkapena(String gako) {
             gakoa = sortuGakoa(gako);
@@ -46,7 +47,25 @@ import java.util.Arrays;
 
         private String sortuGakoa(String gako){
             String g = " ";
-
+            HashSet<Character> lista = new HashSet<Character>();
+            for(int i = 0; i < gako.length(); i++){
+                if(!lista.contains(gako.charAt(i))){
+                    lista.add(gako.charAt(i));
+                    g = g + Character.toString(gako.charAt(i));
+                }
+            }
+            //ASCII kodearen bitartez falta diren karaktereak sartuko ditugu String-ean
+            //A karakterearen ASCII kodea = 65
+            //Z karakterearen ASCII kodea = 90
+            Character c;
+            for(int i = 65; i < 91; i++){
+                c = (char)i;
+                if(!lista.contains(c)){
+                    lista.add(c);
+                    g = g + Character.toString(c);
+                }
+            }
+            System.out.println(g);
             return g;
         }
     }
