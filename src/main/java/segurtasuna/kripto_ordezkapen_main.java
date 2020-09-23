3d9374ace2fa5adda90a11a9ef0296b080599c68
adeiarias <adeiariasupv@gmail.com>
2020-09-23 18:23:20 +0200

    Aldaketa batzuk igo

--- src/main/java/segurtasuna/kripto_ordezkapen_main.java
+++ src/main/java/segurtasuna/kripto_ordezkapen_main.java
@@ -7,12 +7,15 @@ public class kripto_ordezkapen_main {
     }
 
     public static void main(String[] args) {
-        kripto_ordezkapen k = new kripto_ordezkapen();
         Scanner sc = new Scanner(System.in);
         System.out.println("ZER EGIN NAHI DUZU?");
         System.out.println("MEZUA ENKRIPTATZEKO 1 SAKATU");
         System.out.println("MEZUA DESENKRIPTATZEKO 2 SAKATU");
         int n = sc.nextInt();
+        sc.nextLine();
+        System.out.println("ZEIN DA ERABILI NAHI DUZUN GAKOA?");
+        String gako = sc.nextLine();
+        kripto_ordezkapena k = new kripto_ordezkapena(gako);
         sc.nextLine();
         String mezua;
         if (n == 1) {
